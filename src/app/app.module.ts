6da078d8670bfa407bdb551057c9cc22d51600d3
Dmitry-github <dima_moskalenko1902@mail.ru>
2020-11-01 19:48:08 +0300

    Meetings components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeetingsListComponent } from './meetings-list/meetings-list.component';
+import { MeetingsAttendComponent } from './meetings-attend/meetings-attend.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetingsListComponent,
+    MeetingsAttendComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
